24c384a21ac36cf773b61a087ee62b7d22dbcfeb
mr-dargi <dargimohammadreza@gmail.com>
2023-09-07 12:00:10 +0330

    Create Blog project and remove some unuseful files

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+
+
+// Create Route
+// Use App component as root component
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
